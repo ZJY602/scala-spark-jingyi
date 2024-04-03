fd21a8cc3dad0643d1fb2c42f2fff04648f129ca
Jingyi Zhou <jinzhou@univ-paris1.fr>
2024-04-03 16:53:04 +0200

    Modifier Main.scala et pom.xml

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,20 @@
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.SparkConf
+
 object Main extends App {
-  println("Hello, World!")
+  val conf: SparkConf = new SparkConf()
+    .setAppName("Simple Application")
+    .setMaster("local[1]")
+    .set("spark.driver.memory", "1G")
+    // Setting this property is often necessary for testing if you have issues with memory limits
+    .set("spark.testing.memory", "2147480000")
+
+  val sparkSession: SparkSession = SparkSession
+    .builder()
+    .config(conf)
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val inputDF: DataFrame = sparkSession.sql("SELECT 'A' AS column")
+  print(inputDF.show())
 }
